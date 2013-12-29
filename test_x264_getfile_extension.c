5cc2592053f0242672d95ebacc464c0cd1f3c338
zhangbin <commshare.zhang@gmail.com>
2013-12-29 16:09:13 +0800

    test x264 get file extension function

--- test_x264_getfile_extension.c
+++ test_x264_getfile_extension.c
@@ -6,9 +6,10 @@ static inline char *get_filename_extension( char *filename )
     char *ext = filename + strlen( filename );
     while( *ext != '.' && ext > filename )
         ext--;
-    printf("###ext is [%s] \n",ext);
+    printf("###ext is [%s] \n",ext);  //这个时候还是 .264 
+    //== 的优先级，高于+=  
     ext += *ext == '.';
-    return ext;
+    return ext;  //这个时候是264 
 }
 
 int main()
