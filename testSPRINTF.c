6705a9e3ac72f40edf4181033da439b130255769
commshare <commshare.zhang@gmail.com>
2014-01-09 10:31:36 +0800

    add memset and free for testSprintf

--- testSPRINTF.c
+++ testSPRINTF.c
@@ -4,12 +4,16 @@
 
 int main()
 {		
-   char *str=(char *)malloc((1+4+1)*sizeof(char)); //the last 1 byte for '\0' ,4 for int ,1 for char 
-  //memset(str,0,3);
- int n=100;
-  sprintf(str,"%c_%d",'A',n);
-		
-  printf("str is %s\n",str);
+  int i;
+   for(i=0;i<2;i++)
+  {
+      char *str=(char *)malloc((1+4+1)*sizeof(char)); //the last 1 byte for '\0' ,4 for int ,1 for char 
+      memset(str,0,1+4+1);
+      int n=100;
+      sprintf(str,"%c_%d",'A',n);		
+      printf("str is %s\n",str);
+      free(str);
+  }
   return 0;
 
 }
